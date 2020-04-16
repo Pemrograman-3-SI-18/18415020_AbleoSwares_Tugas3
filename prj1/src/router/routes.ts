802d6b1edddc99ad2b9fa61497d691d87eadb4ca
Bamboom11 <bambom92143@gmail.com>
2020-04-16 18:51:53 +0700

    Ableo Swaeres 18415020

---
+++ prj1/src/router/routes.ts
@@ -0,0 +1,28 @@
+import { RouteConfig } from 'vue-router'
+
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '/databuku', component: () => import('pages/home/DataBuku.vue') },
+      { path: '/inputdatabuku', component: () => import('pages/home/InputDataBuku.vue') }]
+  },
+
+  {
+    path: '/',
+    component: () => import('layouts/LoginRegisLayout.vue'),
+    children: [{ path: 'auth/login', component: () => import('pages/users/Login.vue') },
+      { path: 'auth/registrasi', component: () => import('pages/users/Registrasi.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
